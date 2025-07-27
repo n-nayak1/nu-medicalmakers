8f7eb968ff291a43cbbd07d40e76effb9ce7a4d7
Nikhil Nayak <164932985+n-nayak1@users.noreply.github.com>
2025-07-26 22:16:50 -0400

    pancake nav

--- script.js
+++ script.js
@@ -73,3 +73,9 @@ function closeBio() {
   document.getElementById('bio-popup').style.display = 'none';
 }
 
+function toggleMenu() {
+  const menu = document.getElementById("mobile-menu");
+  menu.classList.toggle("show");
+}
+
+
